6108f107f4dcc7a5324c19ad712929cb61f2c6d0
Akshay <dixitakshay3312@gmail.com>
2021-03-11 21:09:29 +0530

    used if else statement to print Hello

--- One.java
+++ One.java
@@ -2,6 +2,18 @@ public class One
 {
 	public static void main(String args[])
 	{
-		System.out.println("Hello");
-	}
+		int a=10;
+		if (a==12)
+		{
+		
+		System.out.println("Hello Akshay");
+
+	   }
+
+	   else
+		{
+			System.out.println("Hello_Sir");
+		}
+}
+
 }
